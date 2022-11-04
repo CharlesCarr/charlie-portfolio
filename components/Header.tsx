6241d3cfda2e5ae6f57d4f58d2fc0964376cc4b1
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-04 14:34:29 -0400

    feat: add styling/hover effect to resume btn

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,8 +41,20 @@ function Header() {
       {/* left header */}
       <div className="flex items-center justify-center">
         <div className="text-xs">{renderThemeChanger()}</div>
-        <button className="border-2 ml-7 border-black dark:border-gray-200 py-1 px-3 text-base sm:text-lg">
+        {/* <button className="border-2 ml-7 border-black dark:border-gray-200 py-1 px-3 text-base sm:text-lg">
           Resume
+        </button> */}
+
+        <button className="ml-5">
+          <a
+            // href="#_"
+            className="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-medium transition-all border-2 border-black dark:border-gray-200 group"
+          >
+            <span className="w-52 h-52 rounded rotate-[-40deg] bg-neutral-800 dark:bg-gray-200 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+            <span className="relative w-full text-left text-neutral-800 dark:text-gray-200 transition-colors duration-300 ease-in-out group-hover:text-gray-200 dark:group-hover:text-neutral-800">
+              Resume
+            </span>
+          </a>
         </button>
       </div>
     </header>
