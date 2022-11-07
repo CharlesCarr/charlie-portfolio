b598154d516ee8e67fc4a93cf19c1f54d9e50d8d
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-07 09:51:37 -0500

    fix: change bg color of headshot

--- components/LeftSection.tsx
+++ components/LeftSection.tsx
@@ -9,7 +9,7 @@ function LeftSection() {
 
   return (
     <section className="w-full lg:w-1/4 h-header flex flex-col sm:flex-col-reverse md:flex-row lg:flex-col justify-center md:justify-around items-center lg:border-r-4 border-black dark:border-gray-200 border-0 border-b-4 lg:border-b-0 px-6 py-6 bg-[#acafb5] dark:bg-neutral-900">
-      <div className="bg-black rounded-2xl w-52 h-60 sm:w-60 sm:mt-5 sm:h-1/2 lg:w-full lg:h-left flex flex-col justify-center items-center relative">
+      <div className="bg-black dark:bg-neutral-800 rounded-2xl w-52 h-60 sm:w-60 sm:mt-5 sm:h-1/2 lg:w-full lg:h-left flex flex-col justify-center items-center relative">
         <Image
           src="/images/headshot.jpg"
           alt="headshot"
