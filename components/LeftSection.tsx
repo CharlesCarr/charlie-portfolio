2e08042be52a7c4e5a0e58c6b38bd00acc630a38
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-06 22:52:20 -0500

    fix: responsive text bug fix

--- components/LeftSection.tsx
+++ components/LeftSection.tsx
@@ -15,12 +15,13 @@ function LeftSection() {
           alt="headshot"
           fill
           priority
+          // min-h-[250px]
           className="rounded-2xl object-cover xl:object-contain overflow-hidden"
         />
       </div>
 
       <div className="w-full lg:w-full sm:w-1/2 sm:h-1/2 h-60 flex flex-col justify-between items-center border-4 border-black dark:border-gray-200 mt-4 px-2 lg:px-4 py-6 shadow-inner bg-[#ced1d3] dark:bg-neutral-700">
-        <p className="text-3xl sm:text-4xl mb-2 tracking-wide text-center">
+        <p className="text-3xl sm:text-2xl 2xl:text-4xl mb-2 tracking-wide text-center">
           CHARLIE CARR
         </p>
         <ul className="w-full flex justify-center items-center m-0 p-0">
@@ -60,13 +61,13 @@ function LeftSection() {
         <div className="w-full h-4 border-b-2 border-black dark:border-gray-200"></div>
 
         <ul className="w-full m-0 p-0 mt-3 flex flex-col justify-center items-center lg:block">
-          <li className="text-sm md:text-base lg:text-xs xl:text-sm font-light mb-2">
+          <li className="text-sm lg:text-xs 2xl:text-sm font-light mb-2">
             Prev. Front End Intern @ Revelio Labs
           </li>
-          <li className="text-sm  md:text-base lg:text-xs xl:text-sm  font-light mb-2">
+          <li className="text-sm lg:text-xs 2xl:text-sm font-light mb-2">
             React / TypeScript Enthusiast
           </li>
-          <li className="text-sm md:text-base lg:text-xs xl:text-sm font-light">
+          <li className="text-sm lg:text-xs 2xl:text-sm font-light">
             Customer Focused
           </li>
         </ul>
--- components/Project.tsx
+++ components/Project.tsx
@@ -14,23 +14,23 @@ const Project = ({ title, image, description, github, demo, tech }: any) => {
   return (
     <div className="h-full md:h-[450px] lg:h-full w-full flex flex-col sm:flex-row justify-center items-center mt-2 sm:mt-0">
       {/* left side project */}
-      <div className="w-full sm:w-1/3 h-64 sm:h-full border-4 border-black dark:border-gray-200 flex flex-col justify-between items-center py-4 lg:py-12 sm:px-2 lg:px-4 mr-0 sm:mr-2 bg-[#acafb5] dark:bg-neutral-800 mb-6 sm:mb-0">
+      <div className="w-full sm:w-1/3 h-64 sm:h-full border-4 border-black dark:border-gray-200 flex flex-col justify-between items-center py-4 2xl:py-10 sm:px-2 lg:px-4 mr-0 sm:mr-2 bg-[#acafb5] dark:bg-neutral-800 mb-6 sm:mb-0">
         <h1 className="font-bold mb-2 text-base lg:text-2xl tracking-wide text-center">
           {title}
         </h1>
         <div className="w-full h-1 border-t-black dark:border-t-gray-200 border-b-0 border-x-0 border-4 mb-2"></div>
-        <p className="text-sm xl:text-base text-center leading-6 xl:leading-8 px-3">{description}</p>
+        <p className="text-sm 2xl:text-base text-center leading-6 2xl:leading-8 px-3">{description}</p>
         <div className="w-full h-4 border-t-black dark:border-t-gray-200 border-b-0 border-x-0 border-4 mt-2"></div>
         <ul className="m-0 p-0 w-full flex justify-center items-center">
           <li>
             <FaGithub
-              className="w-8 h-8 mr-3 cursor-pointer hover:text-black"
+              className="w-8 h-8 mr-3 cursor-pointer dark:hover:text-black"
               onClick={() => gitHubClickHandler()}
             />
           </li>
           <li>
             <HiExternalLink
-              className="w-8 h-8 ml-3 cursor-pointer hover:text-black"
+              className="w-8 h-8 ml-3 cursor-pointer dark:hover:text-black"
               onClick={() => demoClickHandler()}
             />
           </li>
