4a0e63276a37ae7257c7075113b5770ff606c86b
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2023-01-29 13:25:39 -0500

    fix: update 404 route

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
 
 const NotFound = () => {
   const router = useRouter();
@@ -8,11 +7,11 @@ const NotFound = () => {
   useEffect(() => {
     setTimeout(() => {
       router.push("/");
-    }, 3000);
+    }, 2000);
   }, []);
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
+    <div className="w-full h-screen flex flex-col justify-center items-center text-white bg-[#1a1a1a]">
       <p className="text-3xl font-bold">Oooops.....</p>
       <p className="text-2xl">That page cannot be found.</p>
     </div>
